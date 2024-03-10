2776f57e27a33e286c80f33de2f60163cc9a8f7d
Aneel <parmaraneel@protonmail.com>
2024-03-10 16:37:42 -0600

    main now tests collision entry parser

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,8 @@
 import parsing.Parser;
 
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import java.lang.ClassNotFoundException;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Parser parser = new Parser();
-        try
-        {
-            CommandLineParser cmd_parser = new DefaultParser();
-        }
-        catch(Exception e)
-        {
-            System.out.println("Some exception thrown");
-            e.printStackTrace();
-        }
+        Parser.Parse_CSV("collision-databse_1999-2019.csv");
     }
 }
