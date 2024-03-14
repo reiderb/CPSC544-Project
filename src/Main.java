b9cb168c0c28c1ee7f05b9c83849e01dbeea38fc
Reid Erb <reerb@ucalgary.ca>
2024-03-13 21:01:57 -0600

    very little test in main to see how comparing enums, and getting ordinals works

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
 import parsing.CollisionEntryParser;
 import rules.Rule;
+import parsing.CollisionEntry;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        CollisionEntryParser.Parse_CSV("collision-databse_1999-2019.csv");
+        //CollisionEntryParser.Parse_CSV("collision-databse_1999-2019.csv");
         System.out.println("Making empty rule");
         Rule test = new Rule();
+        
+        CollisionEntry.C_WDAY daynum = CollisionEntry.C_WDAY.UNKNOWN;
+        CollisionEntry.C_WDAY[] dayarray = CollisionEntry.C_WDAY.values();
+        
+        int unknowndex = CollisionEntry.C_WDAY.UNKNOWN.ordinal();
+        
+        
+        for (int i = 0; i < dayarray.length; i++)
+        {
+                if (dayarray[i] == daynum)
+                {
+                    System.out.println("that's unknown, alright..");
+                }
+                else
+                {
+                    System.out.println(dayarray[i].toString());
+                }
+                if (i == unknowndex)
+                {
+                    System.out.println("test test");
+                    if (daynum.ordinal() == unknowndex)
+                    {
+                        System.out.println("all as expected..");
+                    }
+                }
+        }
     }
 }
