e8eef2d9ea8def099c7f2111748f99c21a1b4753
Reid Erb <reerb@ucalgary.ca>
2024-03-31 12:19:00 -0600

    ItemSet is also comparable

--- src/apriori/ItemSet.java
+++ src/apriori/ItemSet.java
@@ -3,15 +3,17 @@ package apriori;
 import java.util.ArrayList; //ol' reliable..
 import rules.*;
 
-public class ItemSet
+public class ItemSet implements Comparable<ItemSet>
 {
 	public ArrayList<Predicate> items;
 	public ArrayList<Integer> indices; //indices of CollisionEntry objects containing the given items
+	public int support; //the number of times "items" occur in the database
 	
 	public ItemSet()
 	{
 		items = new ArrayList<Predicate>();
 		indices = new ArrayList<Integer>();
+		support = 0;
 	}
 	
 	public ItemSet(Predicate pred)
@@ -19,5 +21,26 @@ public class ItemSet
 		items = new ArrayList<Predicate>();
 		items.add(pred);
 		indices = new ArrayList<Integer>();
+		support = 0;
+	}
+	
+	@Override
+	public int compareTo(ItemSet obj)
+	{
+		int i = 0;
+		int comp;
+		Predicate pred1, pred2;
+		while (i < this.items.size() && i < obj.items.size())
+		{
+			pred1 = this.items.get(i);
+			pred2 = obj.items.get(i);
+			comp = pred1.compareTo(pred2);
+			if (comp < 0) {return -1;}
+			if (comp > 0) {return 1;}
+			i++;
+		}
+		if (this.items.size() < obj.items.size()) {return -1;}
+		if (this.items.size() > obj.items.size()) {return 1;}
+		return 0;
 	}
 }
