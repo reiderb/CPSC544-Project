b5ec5115d167a977d8f1a3cfb25842f28dfc27f5
Aneel <parmaraneel@protonmail.com>
2024-04-02 13:23:47 -0600

    add more synchronization points to remove item set duplication bug

--- src/apriori/IndexedApriori.java
+++ src/apriori/IndexedApriori.java
@@ -117,13 +117,21 @@ public class IndexedApriori
 									// System.out.println("Sorted predicate list");
 									Item = new ItemSet();
 									Item.items = predList;
-									if (!doesItemSetExist(Item, newlist)) // check that we haven't already made an itemset with these predicates
+									boolean doesItemSetExist;
+									synchronized(newlist)
+									{
+										doesItemSetExist = doesItemSetExist(Item, newlist);
+									}
+									if (!doesItemSetExist) // check that we haven't already made an itemset with these predicates
 									{
 										Indices = reconstructIndices(Item, mincov);
 										// System.out.println("Found intersection of indices");
 										if (Indices.size() >= mincov) {
 											Item.support = Indices.size();
-											Item.blacklist = joinBlacklists(oneitem.get(j), prevlist.get(i));
+											synchronized(prevlist.get(i))
+											{
+												Item.blacklist = joinBlacklists(oneitem.get(j), prevlist.get(i));
+											}
 											Item.blacklist = (prevlist.get(i).blacklist);
 											synchronized(newlist)
 											{
@@ -131,10 +139,13 @@ public class IndexedApriori
 												Collections.sort(newlist);
 											}
 										} else {
-											prevlist.get(i).blacklist.add(oneitem.get(j));
-											if (prevlist.get(i).items.size() == 1) {
-												oneitem.get(j).blacklist.add(prevlist.get(i));
+											synchronized (prevlist.get(i).blacklist) {
+												prevlist.get(i).blacklist.add(oneitem.get(j));
+												if (prevlist.get(i).items.size() == 1) {
+													oneitem.get(j).blacklist.add(prevlist.get(i));
+												}
 											}
+
 										}
 									}
 								}
